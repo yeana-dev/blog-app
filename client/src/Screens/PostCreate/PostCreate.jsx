edb024b27bc7de6c9d1038c5d6f6344cda71d4a5
Sofanit Tesfaye <sofanittesfaye@Sofanits-MacBook-Pro.local>
2021-09-08 14:59:12 -0400

    added postCreate and postEdit

---
+++ client/src/Screens/PostCreate/PostCreate.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import "./PostCreate.css";
+import Layout from "../../components/Layout/Layout";
+import { Redirect } from "react-router-dom";
+import { createPost } from "../../services/products";
+
+const PostCreate = (props) => {
+  const [post, setPost] = useState({
+    title: "",
+    author: "",
+    category: "",
+    imgURL: "",
+    article: "",
+  });
+
+  const [isCreated, setCreated] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createPost(post);
+    setCreated({ created });
+  };
+
+  if (isCreated) {
+    return <Redirect to={`/posts`} />;
+  }
+  return (
+    <Layout>
+      <form className="create-form" onSubmit={handleSubmit}>
+        <input
+          className="input-title"
+          placeholder="title"
+          value={post.title}
+          name="title"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <input
+          className="input-author"
+          placeholder="author"
+          value={post.author}
+          name="author"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-category"
+          placeholder="category"
+          value={post.category}
+          name="category"
+          required
+          onChange={handleChange}
+        />
+        <input
+          className="input-image-link"
+          placeholder="Image Link"
+          value={post.imgURL}
+          name="imgURL"
+          required
+          onChange={handleChange}
+        />
+
+        <textarea
+          className="textarea-article"
+          rows={10}
+          placeholder="article"
+          value={post.article}
+          name="article"
+          required
+          onChange={handleChange}
+        />
+
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </Layout>
+  );
+};
+
+export default PostCreate;
